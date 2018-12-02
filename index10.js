3b21a40b4769bf7f47375c6096eda466e37e6cf9
VladimirLopatin <43500489+VladimirLopatin@users.noreply.github.com>
2018-12-02 22:20:01 +0300

    IT-K. JS and CSS to external files.

---
+++ index10.js
@@ -0,0 +1,14 @@
+    var firstNameId = 'first-name',
+        firstNameEl = document.getElementById(firstNameId),
+        lastNameId = 'lastName',
+        lastNameEl = document.getElementById(lastNameId),
+        addressId = 'address',
+        addressEl = document.getElementById(addressId),
+        citiesId = 'cities',
+        citiesEl = document.getElementById(citiesId),
+        hobbiesId = 'hobbies',
+        hobbiesEl = document.getElementById(hobbiesId);
+
+
+    firstNameEl.value = 'Andrew';
+    lastNameEl.className = 'last-name-input default-input error-input';
